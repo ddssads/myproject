5b973ae70d012a4cdd8d5360f739617fe4d3e03c
ddssads <tw0937608360@gmail.com>
2020-07-24 16:52:10 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/sc/Hello.java
+++ src/main/java/com/sc/Hello.java
@@ -5,6 +5,17 @@ public class Hello {
 //        System.out.println("Hello 妳好嗎");
         Person p = new Person();
         p.hello();
+        int a = 19;
+        short a1;
+        long a2;
+        byte b = 120;
+        char c = 'A';
+        float weight = 55.9f;
+        double height = 166.7;
+        boolean adult = true;
+        boolean enroll = false;
+
+        String name = "Java";
     }
 
 
--- src/main/kotlin/com/sc/Hello.kt
+++ src/main/kotlin/com/sc/Hello.kt
@@ -3,8 +3,17 @@ package com.sc
 fun main() {
 //    println("Hello Kotlin")
 //    Human().hello()
-    val h = Human()
+    var h = Human()
     h.hello()
+    val age = 19//val不能改變;kotlin自動推斷變數資料型態
+    //age = 20 val不能改變 報錯
+    var weight = 66.5f//var可以改變
+    var height : Int;
+    height = 177;
+    val adult = true;
+    var name : String
+    name = "Kotlin"
+    println("name:" + name + "age:" + age + "weight:" + weight + "height:" + height + "adult:" + adult)
 }
 
 class Human{
