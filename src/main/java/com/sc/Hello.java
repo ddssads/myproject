fd75713f4239558be1c7b81e976badcf95c4851a
ddssads <tw0937608360@gmail.com>
2020-07-24 15:33:11 +0800

    Initial commit

---
+++ src/main/java/com/sc/Hello.java
@@ -0,0 +1,11 @@
+package com.sc;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello 妳好嗎");
+        Person p = new Person();
+        p.hello();
+    }
+
+
+}
---
+++ src/main/java/com/sc/Person.java
@@ -0,0 +1,7 @@
+package com.sc;
+
+public class Person {
+    public void hello(){
+        System.out.println("Hello world");
+    }
+}
